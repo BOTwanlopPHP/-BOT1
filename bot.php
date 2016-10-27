e47826c84050ccb08d2f36d492cdec63f8e7c3cc
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-10-27 14:55:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,15 @@ curl_setopt($ch, CURLOPT_URL, "http://192.168.1.99 ");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POST, true);
 
-		if ($t4 >=26);
+		if ($t4 >=26){}
+		else{
+		}
+		"message": {
+          
+             "type": "text",
+             "text": "Hello, world"
+          }
+		
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
