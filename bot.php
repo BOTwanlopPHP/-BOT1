81c02e70f63a7e1b9986b63d45c1fee2e15d368c
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-10-27 13:17:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 $access_token = 'MheeIpqJUmhyvXW91x/oygbvPBHtBChGseqGlYYcF5Z43ndEYW8QqfaDYGfY2wVPxTN3CQHv1Rf+lVvJ71FlK9kWOnvJ+xgjkyo5R9DdH0K2mnF/rTtiNLk3vniRE62CiGay5kofMlzpqY0oRAfMMgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
-$content = file_get_contents('php://input');
+$content = file_get_contents('http://192.168.1.99');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
