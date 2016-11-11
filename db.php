a52f39e0b45f0a2219eaf7fe1e50b783b64d925d
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-11 11:54:33 +0700

    Create db.php

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+class DB {
+  // initial connection
+  public static $dbName = 'bee';
+  public static $user = 'root';
+  public static $password = 'root';
+  public static $host = 'localhost';
+  public static $port = 3306; //hhvm complains if this is null
+  public static $encoding = 'room1';
+  ?>
