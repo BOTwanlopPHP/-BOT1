88b7efa35af7e1e51b17dcfac8fea57efb0ccfa3
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-13 11:49:09 +0700

    Update db.php

--- db.php
+++ db.php
@@ -2,8 +2,11 @@
 require_once('db.php');
 
 try {
-    $mysql = new MySQL('host', 'root', 'root', 'bee');
-} catch (Exception $e) {
+    $mysql = new MySQL('host', 'root', 'root');
+    $mysql->use_db($bee);
+} catch (Exception $e) 
+{
     error($e->getMessage());
 }
+
 ?>
