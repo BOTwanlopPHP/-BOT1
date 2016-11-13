d5fde445f574e6bc6c233709f7d88deb4423691e
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-13 11:55:09 +0700

    Update db.php

--- db.php
+++ db.php
@@ -3,8 +3,6 @@ $dbname = 'bee';
 $dbuser = 'root';
 $dbpass = 'root';
 $dbhost = 'host';
-$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysql_select_db($dbname) or die("Could not open the db '$dbname'");
 
 
 
