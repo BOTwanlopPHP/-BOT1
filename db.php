53ad86acec8a28d384b36e74557385b72be06af9
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-13 11:58:12 +0700

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
 $dbname = 'bee';
 $dbuser = 'root';
 $dbpass = 'root';
-$dbhost = 'host';
+$dbhost = 'room1';
 $connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysql_select_db($dbname) or die("Could not open the db '$dbname'");
 $test_query = "SHOW TABLES FROM $dbname";
