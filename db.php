15b7a39bca74ae9124c2035feb343763efcc1097
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-13 11:52:50 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,18 @@
 <?php
-require_once('db.php');
+$dbname = 'bee';
+$dbuser = 'root';
+$dbpass = 'root';
+$dbhost = 'room1';
+$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysql_select_db($dbname) or die("Could not open the db '$dbname'");
+$test_query = "SHOW TABLES FROM $dbname";
+$result = mysql_query($test_query);
+$tblCnt = 0;
+while($tbl = mysql_fetch_array($result)) {
+  $tblCnt++;
+  #echo $tbl[0]."<br />\n";
+}
 
-try {
-    $mysql = new MySQL('host', 'root', 'root');
-    $mysql->use_db($bee);
-} 
+
 
 
