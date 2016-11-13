6735916a332d99bae6dd7b68146e721027302952
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-13 11:41:34 +0700

    Update db.php

--- db.php
+++ db.php
@@ -6,5 +6,5 @@ class DB {
   public static $password = 'root';
   public static $host = 'localhost';
   public static $port = 3306; //hhvm complains if this is null
-  
+  public static $encoding = 'latin1';
 ?>
