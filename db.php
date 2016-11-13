232ef6d3cb9ede39f04a54c4215bee868ac688ba
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-13 11:45:44 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,9 @@
 <?php
-class DB {
-  // initial connection
-  public static $dbName = 'bee';
-  public static $user = 'root';
-  public static $password = 'root';
-  public static $host = 'localhost';
-  public static $port = 80; //hhvm complains if this is null
-  public static $encoding = 'latin1';
+require_once('MySQL.php');
+
+try {
+    $mysql = new MySQL('host', 'root', 'root', 'bee');
+} catch (Exception $e) {
+    error($e->getMessage());
+}
 ?>
