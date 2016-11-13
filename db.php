3b3cb5bdb1a0c210e364934408b99b2c49c6dcff
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-13 11:54:43 +0700

    Update db.php

--- db.php
+++ db.php
@@ -5,13 +5,7 @@ $dbpass = 'root';
 $dbhost = 'host';
 $connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysql_select_db($dbname) or die("Could not open the db '$dbname'");
-$test_query = "SHOW TABLES FROM $dbname";
-$result = mysql_query($test_query);
-$tblCnt = 0;
-while($tbl = mysql_fetch_array($result)) {
-  $tblCnt++;
-  echo $tbl[0]."<br />\n";
-}
+
 
 
 
