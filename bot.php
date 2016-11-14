2b84abb109daad5f3dedb06d1156de7cf6064634
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-14 16:20:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,25 @@
 <?php
+error_reporting(0);
+function runSQL($rsql) {
+    $hostname = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname   = "bee";
+    $connect = mysql_connect($hostname,$username,$password) or die ("Error: could not connect to database");
+    $db = mysql_select_db($dbname);
+    mysql_query("set character set utf8");     
+    $result = mysql_query($rsql) or die ('Error: could not query data' . $rsql); 
+    return $result;
+    mysql_close($connect);
+}
+function countRec($fname,$tname,$where) {
+    $sql = "SELECT count($fname) FROM $tname $where";
+    $result = runSQL($sql);
+    while ($row = mysql_fetch_array($result)) {
+        return $row[0];
+    }
+}
+######################################################
 $access_token = 'MheeIpqJUmhyvXW91x/oygbvPBHtBChGseqGlYYcF5Z43ndEYW8QqfaDYGfY2wVPxTN3CQHv1Rf+lVvJ71FlK9kWOnvJ+xgjkyo5R9DdH0K2mnF/rTtiNLk3vniRE62CiGay5kofMlzpqY0oRAfMMgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
