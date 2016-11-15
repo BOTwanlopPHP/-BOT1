9bb766d89c2b6cd6b50d82066855b994b89b6cdb
BOTwanlopPHP <BOTwanlopPHP@gmail.com>
2016-11-15 16:11:00 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,20 +1,78 @@
 <?php
-$host = "localhost"; // ชื่อ host หรือ ip ที่ใช้
-$userhost = "root"; // ชื่อ user ที่ใช้ในการล็อกอิน
-$passhost = "root"; // password ที่ใช้ในการล็อกอิน
-$database = "bee"; // ชื่อ Database
-$conn = mysql_connect($host,$userhost,$passhost);
-if(!$conn){
-	echo "ไม่สามารถเชื่อมต่อฐานข้อมูลได้";
-}
-mysql_query("use $database"); // เลือกฐานข้อมูลที่ใช้
-mysql_query("SET character_set_results=utf8");
-mysql_query("SET character_set_client=utf8");
-mysql_query("SET character_set_connection=utf8");
 
-$query = "select * from room1";
-$data = mysql_query($query); //query ข้อมูล
-while($show = mysql_fetch_object($data)){
-	echo "$show->id." ".$show->curdate." ".$show->curtime." ".$show->status.";
+ 
+class DB {
+     
+    public $connect;
+    public $result;
+    public $recode = array();
+     
+    public $type      = 'mysql';
+    public $server    = 'localhost';
+    public $username  = 'root';
+    public $password  = 'root';
+    public $dbname    = 'bee';
+    public $port      = 3306;
+    public $charset   = 'utf8';
+     
+    public function __construct() {
+        $this->connect  = mysql_connect( $this->server, $this->username, $this->password )
+            or die( "Error Connect to Database" );
+        mysql_select_db( $this->dbname, $this->connect ) or die( "Error Connect to Table" );
+        mysql_query( "SET NAMES UTF8" );
+    }
+     
+    // ประมวลผลคำสั่ง SQL
+    public function query($txtSQL = ''){
+        if(!empty($txtSQL)){
+            $this->result = mysql_query( $txtSQL , $this->connect );
+            return $this;
+        }else{
+            return false;
+        }
+    }
+    public function getStatus(){
+        if(!empty($this->result)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+     
+    /* ==========================================================
+     * ดึงข้อมูล SELECT
+     ============================================================ */
+     
+    // รายการเดียว
+    public function find(){
+        if(!empty($this->result)){
+            $this->recode   = mysql_fetch_object( $this->result );
+            return $this->recode;
+        }else{
+            return false;
+        }
+    }
+    // รายการทั้งหมด
+    public function findAll(){
+        if(!empty($this->result)){
+            $record = array();
+            while ($row = mysql_fetch_array( $this->result , MYSQL_ASSOC)) {
+                $record[] = (object) $row;
+            }
+            return $record;
+        }else{
+            return false;
+        }
+    }
+    
+    // จำนวน Record
+    public function count(){
+        if(!empty($this->result)){
+            return mysql_num_rows( $this->result );
+        }else{
+            return false;
+        }
+    }
+     
 }
 ?>
